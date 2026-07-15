bc612bc62dbe2202f3b9d2d3b2febfdaf3cadf0c
Amel GUENNINECHE <96930086+amelguennineche@users.noreply.github.com>
2026-07-15 21:14:23 +0200

    MAJ ETL pipeline

--- src/etl_pipeline.py
+++ src/etl_pipeline.py
@@ -7,6 +7,12 @@ RAW_DATA_DIR = BASE_DIR / "data" / "raw"
 
 STAGING_DIR = BASE_DIR / "data" / "staging"
 
+
+
+# ==========================
+# Extrat Functions
+# ==========================
+
 def extract_products():
 
     file_path = RAW_DATA_DIR / "products.csv"
@@ -22,6 +28,18 @@ def extract_factories():
     df = pd.read_csv(file_path)
 
     return df
+
+def extract_production_orders():
+
+    file_path = RAW_DATA_DIR / "production_orders.csv"
+
+    df = pd.read_csv(file_path)
+
+    return df
+
+# ==========================
+# Transform Functions
+# ==========================
 
 def transform_products(df):
 
@@ -47,7 +65,7 @@ def transform_products(df):
         df["launch_date"]
     )
 
-        # Data quality checks
+# Data quality checks
 
     assert df["product_id"].is_unique
 
@@ -116,11 +134,66 @@ def transform_factories(df):
         df["employees_count"] > 0
     ).all()
 
-    
+    return df
 
+def transform_production_orders(df):
+
+    df["production_date"] = pd.to_datetime(
+        df["production_date"]
+    )
+
+    df["production_status"] = (
+        df["production_status"]
+        .str.strip()
+        .str.title()
+    )
+
+    assert df["production_id"].is_unique
+
+    assert df["product_id"].notnull().all()
+
+    assert df["factory_id"].notnull().all()
+
+    assert df["production_date"].notnull().all()
+
+    assert df["planned_quantity"].notnull().all()
+
+    assert df["actual_quantity"].notnull().all()
+
+    assert (
+        df["planned_quantity"] > 0
+    ).all()
+
+    assert (
+        df["actual_quantity"] > 0
+    ).all()
+
+    assert (
+        df["defect_rate"].between(0, 1)
+    ).all()
+
+
+    # Allowed production statuses
+
+    allowed_status = [
+        "Completed",
+        "In Progress",
+        "Delayed"
+    ]
+
+
+    assert (
+        df["production_status"]
+        .isin(allowed_status)
+        .all()
+    )
 
 
     return df
+
+# ==========================
+# Load Functions
+# ==========================
 
 def load_products(df):
 
@@ -168,7 +241,32 @@ def load_factories(df):
         "Factories staging loaded"
     )
 
+def load_production_orders(df):
+
+    STAGING_DIR.mkdir(
+        exist_ok=True
+    )
+
+
+    output_file = (
+        STAGING_DIR /
+        "stg_production_orders.csv"
+    )
+
+
+    df.to_csv(
+        output_file,
+        index=False
+    )
+
+
+    print(
+        "Production orders staging loaded"
+    )
+
 if __name__ == "__main__":
+
+    # Products
 
     """products = extract_products()
 
@@ -176,8 +274,22 @@ if __name__ == "__main__":
 
     load_products(products)"""
 
-    factories = extract_factories()
+    # Factories
+
+    """factories = extract_factories()
 
     factories = transform_factories(factories)
 
-    load_factories(factories)
+    load_factories(factories)"""
+
+    # Production orders
+
+    production_orders = extract_production_orders()
+
+    production_orders = transform_production_orders(
+        production_orders
+    )
+
+    load_production_orders(
+        production_orders
+    )
