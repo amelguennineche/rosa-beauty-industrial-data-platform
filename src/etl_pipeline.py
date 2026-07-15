922581da1ee500b1365eb07fdefb5a3dd3ba6d83
Amel GUENNINECHE <96930086+amelguennineche@users.noreply.github.com>
2026-07-15 19:30:25 +0200

    Add factories and products staging transformation pipeline

---
+++ src/etl_pipeline.py
@@ -0,0 +1,183 @@
+import pandas as pd
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+RAW_DATA_DIR = BASE_DIR / "data" / "raw"
+
+STAGING_DIR = BASE_DIR / "data" / "staging"
+
+def extract_products():
+
+    file_path = RAW_DATA_DIR / "products.csv"
+
+    df = pd.read_csv(file_path)
+
+    return df
+
+def extract_factories():
+
+    file_path = RAW_DATA_DIR / "factories.csv"
+
+    df = pd.read_csv(file_path)
+
+    return df
+
+def transform_products(df):
+
+    df["product_name"] = (
+        df["product_name"]
+        .str.strip()
+    )
+
+
+    df["category"] = (
+        df["category"]
+        .str.title()
+    )
+
+
+    df["brand"] = (
+        df["brand"]
+        .str.title()
+    )
+
+
+    df["launch_date"] = pd.to_datetime(
+        df["launch_date"]
+    )
+
+        # Data quality checks
+
+    assert df["product_id"].is_unique
+
+    assert df["product_name"].notnull().all()
+
+    assert df["category"].notnull().all()
+
+    assert df["brand"].notnull().all()
+
+    assert df["launch_date"].notnull().all()
+
+    return df
+
+def transform_factories(df):
+
+    # Nettoyage des textes
+
+    df["factory_name"] = (
+        df["factory_name"]
+        .str.strip()
+    )
+
+
+    df["city"] = (
+        df["city"]
+        .str.title()
+    )
+
+
+    df["region"] = (
+        df["region"]
+        .str.strip()
+    )
+
+
+    df["country"] = (
+        df["country"]
+        .str.title()
+    )
+
+
+    # Conversion date
+
+    df["opening_date"] = pd.to_datetime(
+        df["opening_date"]
+    )
+
+
+    # Data quality checks
+
+    assert df["factory_id"].is_unique
+
+    assert df["factory_name"].notnull().all()
+
+    assert df["city"].notnull().all()
+
+    assert df["region"].notnull().all()
+
+    assert df["opening_date"].notnull().all()
+
+    assert (
+        df["production_capacity"] > 0
+    ).all()
+
+    assert (
+        df["employees_count"] > 0
+    ).all()
+
+    
+
+
+
+    return df
+
+def load_products(df):
+
+    STAGING_DIR.mkdir(
+        exist_ok=True
+    )
+
+
+    output_file = (
+        STAGING_DIR /
+        "stg_products.csv"
+    )
+
+
+    df.to_csv(
+        output_file,
+        index=False
+    )
+
+
+    print(
+        "Products staging loaded"
+    )
+
+def load_factories(df):
+
+    STAGING_DIR.mkdir(
+        exist_ok=True
+    )
+
+
+    output_file = (
+        STAGING_DIR /
+        "stg_factories.csv"
+    )
+
+
+    df.to_csv(
+        output_file,
+        index=False
+    )
+
+
+    print(
+        "Factories staging loaded"
+    )
+
+if __name__ == "__main__":
+
+    """products = extract_products()
+
+    products = transform_products(products)
+
+    load_products(products)"""
+
+    factories = extract_factories()
+
+    factories = transform_factories(factories)
+
+    load_factories(factories)
